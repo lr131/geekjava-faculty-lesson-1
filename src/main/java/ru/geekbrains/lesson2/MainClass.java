64da5d20a7d68350186c13c905c5a1561cfa7aa4
Kristina Retivykh <lr131k@gmail.com>
2020-12-10 08:53:17 +0800

    Add draft methods and todo-notes for homework-2

---
+++ src/main/java/ru/geekbrains/lesson2/MainClass.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.lesson2;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public static void printArray(int array) {
+
+    }
+
+    public static void printMatrix(){
+
+    }
+
+}
